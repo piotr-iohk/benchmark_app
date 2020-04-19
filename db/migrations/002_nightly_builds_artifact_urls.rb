d2512c4aaeaac0a48f3ea59eb834e6fc8e9a67f3
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-04-19 13:16:08 +0200

    drop unnecessary migrations

--- db/migrations/002_nightly_builds_artifact_urls.rb
+++
@@ -1,9 +0,0 @@
-Sequel.migration do
-  change do
-    add_column :testnet_restores, :artifact_txt_url, String
-    add_column :testnet_restores, :artifact_svg_url, String
-
-    add_column :mainnet_restores, :artifact_txt_url, String
-    add_column :mainnet_restores, :artifact_svg_url, String
-    end
-end
