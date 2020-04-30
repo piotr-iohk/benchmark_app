55b6350e6bfe9968f81761d9783ad39c5962cfd9
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-05-01 01:04:03 +0200

    Insert test data in integration tests

--- tests/integration_spec.rb
+++ tests/integration_spec.rb
@@ -3,11 +3,33 @@ require_relative 'spec_helper'
 require 'rspec'
 require 'rack/test'
 
-describe 'The HelloWorld App' do
+describe 'The Benchmark App' do
   include Rack::Test::Methods
 
   def app
     BenchmarkApp
+  end
+
+  before(:all) do
+    # Insert data
+    DB = Sequel.connect(DB_PATH)
+    testnet = File.read("#{Dir.pwd}/tests/artifacts/restoration-testnet.txt")
+    mainnet = File.read("#{Dir.pwd}/tests/artifacts/restoration-mainnet.txt")
+    latency = File.read("#{Dir.pwd}/tests/artifacts/latency.log")
+
+    build = { build_no: 111,
+              datetime: Time.now,
+              rev: "2e258c3e5852dd758a7a81dc8be35bd729c58241"
+            }
+    mainnet_results = Readers::Restorations.read_to_hash mainnet, "mainnet"
+    testnet_results = Readers::Restorations.read_to_hash testnet, "testnet"
+    latency_results = Readers::Latencies.read_to_hash latency
+    results = { build: build,
+                mainnet_restores: mainnet_results,
+                testnet_restores: testnet_results,
+                latency_results: latency_results
+              }
+    2.times { Helpers::DataTransfer.insert_into_db(results, DB) }
   end
 
   it "Main" do
@@ -27,6 +49,7 @@ describe 'The HelloWorld App' do
 
   it "Nightbuilds/1" do
     get '/nightbuilds/1'
+    get '/nightbuilds/111'
     # expect(last_response).to be_ok
   end
 
