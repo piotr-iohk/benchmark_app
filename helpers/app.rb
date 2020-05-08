2cec15e3f433f950311529424f0ccc8a1e251f36
Piotr Stachyra <piotr.stachyra@iohk.io>
2020-05-08 08:11:34 +0200

    Fix buildkite link

--- helpers/app.rb
+++ helpers/app.rb
@@ -3,14 +3,14 @@ module Helpers
     GH_URL = "https://github.com/input-output-hk/cardano-wallet/commit"
     BK_URL = "https://buildkite.com/input-output-hk/cardano-wallet-nightly/builds"
 
-    def build_status(status)
+    def build_status(status, build_no)
       case status
       when "failed" then cl = "bg-danger"
       when "canceled" then cl = "bg-warning"
       when "passed" then cl = "bg-success"
       else cl = "bg-warning"
       end
-      "<a href='#{BK_URL}/#{status}' class=\"d-inline p-2 #{cl} text-white\">#{status}</a>"
+      "<a href='#{BK_URL}/#{build_no}' class='d-inline p-2 #{cl} text-white' target='_blank'>#{status}</a>"
     end
 
     def link_to_nb id
